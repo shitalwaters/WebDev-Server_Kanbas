b38fc2b5c4dd2313dfcc247021c15b89143fbfb1
shetalw <sdwaters8@gmail.com>
2023-12-05 14:57:25 -0500

    Reorganize repository structure and created new repo serperate from react.js repo

--- 2023/fall/webdev/kanbas-react-web-app-2-a4-branch-11242023/src/Kanbas/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import modulesReducer from "../Courses/Modules/modulesReducer";
-const store = configureStore({
-reducer: {
-modulesReducer
-}
-});
-export default store;
